78b9fb9b4d07588c213d546dac4b7448358c4915
rizquadnan <madnanrizqullah@gmail.com>
2023-09-22 23:47:32 +0300

    add frontend cors

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,18 @@ import { routes } from './routes';
 import cookieParser from "cookie-parser";
 
 const EXPRESS_PORT = process.env.PORT; 
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-  credentials: true,
-  origin: ["http://localhost:3000"]
-}))
+app.use(
+  cors({
+    credentials: true,
+    origin: [FRONTEND_URL],
+  })
+);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('INFO :: Root route called');
